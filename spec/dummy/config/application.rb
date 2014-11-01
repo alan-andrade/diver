06a04483eaef15cd6e68432a50467987199ef212
Alan Andrade <alan.andradec@gmail.com>
2014-10-31 23:07:51 -0700

    cut rails components down

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -1,6 +1,8 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+# require 'rails/all'
+require 'action_controller/railtie'
+require 'action_view/railtie'
 
 Bundler.require(*Rails.groups)
 require "diver"
